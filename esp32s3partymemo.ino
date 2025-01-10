a1b6bca7f2e90c7cb6021eb519adfce2508968af
MichalMroz06 <michalmroz2006@gmail.com>
2025-01-10 10:02:18 +0100

    Working tact swich on GPIO21 with built-in LED
    Code for tact swich for future part of project. Btn is on GPIO21 and for presention of working button is used built-in LED. Also in png file is attached schema of how is connect tact swich to board.

--- esp32s3partymemo.ino
+++ esp32s3partymemo.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+#define BUTTON_PIN 21
 
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(21, INPUT_PULLUP); //Btn on GPIO21
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+#ifdef RGB_BUILTIN
 
+  int btnState = !digitalRead(BUTTON_PIN);
+
+  if( btnState == 1 ){
+    digitalWrite(RGB_BUILTIN, HIGH);
+  }else{
+    digitalWrite(RGB_BUILTIN, LOW);
+  }
+
+#endif
 }
